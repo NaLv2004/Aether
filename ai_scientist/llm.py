82631031c014531ecdc37bc2dce4b451246799dc
NaLv2004 <213221582@seu.edu.cn>
2026-03-03 14:57:07 +0800

    modify api acquisition

--- ai_scientist/llm.py
+++ ai_scientist/llm.py
@@ -9,58 +9,6 @@ import openai
 
 MAX_NUM_TOKENS = 1000000
 
-AVAILABLE_LLMS = [
-    # Anthropic models
-    "claude-3-5-sonnet-20240620",
-    "claude-3-5-sonnet-20241022",
-    # OpenAI models
-    "gpt-4o-mini",
-    "gpt-4o-mini-2024-07-18",
-    "gpt-4o",
-    "gpt-4o-2024-05-13",
-    "gpt-4o-2024-08-06",
-    "gpt-4.1",
-    "gpt-4.1-2025-04-14",
-    "gpt-4.1-mini",
-    "gpt-4.1-mini-2025-04-14",
-    "gpt-4.1-nano",
-    "gpt-4.1-nano-2025-04-14",
-    "o1",
-    "o1-2024-12-17",
-    "o1-preview-2024-09-12",
-    "o1-mini",
-    "o1-mini-2024-09-12",
-    "o3-mini",
-    "o3-mini-2025-01-31",
-    # OpenRouter models
-    "llama3.1-405b",
-    # Anthropic Claude models via Amazon Bedrock
-    "bedrock/anthropic.claude-3-sonnet-20240229-v1:0",
-    "bedrock/anthropic.claude-3-5-sonnet-20240620-v1:0",
-    "bedrock/anthropic.claude-3-5-sonnet-20241022-v2:0",
-    "bedrock/anthropic.claude-3-haiku-20240307-v1:0",
-    "bedrock/anthropic.claude-3-opus-20240229-v1:0",
-    # Anthropic Claude models Vertex AI
-    "vertex_ai/claude-3-opus@20240229",
-    "vertex_ai/claude-3-5-sonnet@20240620",
-    "vertex_ai/claude-3-5-sonnet-v2@20241022",
-    "vertex_ai/claude-3-sonnet@20240229",
-    "vertex_ai/claude-3-haiku@20240307",
-    # DeepSeek models
-    "deepseek-chat",
-    "deepseek-coder",
-    "deepseek-reasoner",
-    # Google Gemini models
-    "gemini-1.5-flash",
-    "gemini-1.5-pro",
-    "gemini-2.0-flash",
-    "gemini-2.0-flash-lite",
-    "gemini-2.0-flash-thinking-exp-01-21",
-    "gemini-2.5-pro-preview-03-25",
-    "gemini-2.5-pro-exp-03-25",
-]
-
-
 class LLMAgent:
     def __init__(self, model, temperature=0.75, log_file="llm_agent.log"):
         """
@@ -150,7 +98,7 @@ class LLMAgent:
         elif "gemini" in model:
             print(f"Using OpenAI API with {model}.")
             return openai.OpenAI(
-                api_key = "sk-VQ7JeuIO1tr2LmJhZRc8BzSNPyUbYnAYKISvt6XH2PHCTKr8",
+                api_key = os.environ.get("GEMINI_API_KEY"),
                 base_url="https://newapi.baosiapi.com/v1"
             ), model
         else:
